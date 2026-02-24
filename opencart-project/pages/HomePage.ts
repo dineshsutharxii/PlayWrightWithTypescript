38a28f14723972501ffc97518b89f00c09f75ce5
dinesh <dineshsutharxii@gmail.com>
2026-02-24 17:22:43 +0530

    Added RegistrationPage

--- opencart-project/pages/HomePage.ts
+++ opencart-project/pages/HomePage.ts
@@ -22,5 +22,64 @@ export class HomePage{
     }
 
     //action methods
+    //Check if HomePage exists
+    async isHomePageExists(){
+        let title = await this.page.title();
+        if (title) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    //Click 'My Account' link
+    async clickMyAccount(){
+        try {
+            await this.linkMyAccount.click();
+        } catch (error) {
+            console.log("Error while clicking my account : ", error)
+            throw error;
+        }
+    }
+
+    //Click 'Register'
+    async clickRegister(){
+        try {
+            await this.linkRegister.click();
+        } catch (error) {
+            console.log("Error while clicking Register : ", error)
+            throw error;
+        }
+    }
+
+    //Click 'Login'
+    async clickLogin(){
+        try {
+            await this.linkLogin.click();
+        } catch (error) {
+            console.log("Error while clicking Login : ", error)
+            throw error;
+        }
+    }
+
+    //Enter product Name
+    async enterProductName(prodctName:string){
+        try {
+            await this.txtSearchbox.fill(prodctName);
+        } catch (error) {
+            console.log("Error while entering product Name : ", error)
+            throw error;
+        }
+    }
+
+    //Click Search button
+    async clickSeach(prodctName:string){
+        try {
+            await this.btnSearch.click();
+        } catch (error) {
+            console.log("Error while clicking Search button : ", error)
+            throw error;
+        }
+    }
 
 }
