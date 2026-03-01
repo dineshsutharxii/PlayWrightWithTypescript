84ed1f88de5761a3b3f0fd8b7f601c9079bcf6e3
dinesh <dineshsutharxii@gmail.com>
2026-03-01 16:59:57 +0530

    Added more pages and tests

--- opencart-project/utils/dataProvider.ts
+++ opencart-project/utils/dataProvider.ts
@@ -4,12 +4,12 @@ import {parse} from 'csv-parse/sync'
 export class DataProvider{
 
     static getTestDataFromJson(filePath:string){
-        let data:string = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+        let data:any = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
         return data;
     }
 
     static getTestDataFromCsv(filePath:string){
-        let data = parse(fs.readFileSync(filePath), {columns:true, skip_empty_lines:true});
+        let data:any = parse(fs.readFileSync(filePath), {columns:true, skip_empty_lines:true});
         return data; 
     }
 }
